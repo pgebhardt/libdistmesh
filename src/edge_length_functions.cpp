0241adcc377caab87e45d9d9a3746293afcd14de
schansge <grosser.knuff@googlemail.com>
2013-08-09 08:06:39 +0200

    uniform edge length function used build in Ones method

--- src/edge_length_functions.cpp
+++ src/edge_length_functions.cpp
@@ -5,13 +5,11 @@
 
 #include "distmesh/distmesh.h"
 
-// creates distance function for circular domains
+// uniform edge length
 std::function<distmesh::dtype::array<distmesh::dtype::real>(
     distmesh::dtype::array<distmesh::dtype::real>&)>
     distmesh::edge_length_functions::uniform() {
     return [=](dtype::array<dtype::real>& points) {
-        dtype::array<dtype::real> result(points.rows(), 1);
-        result.fill(1.0);
-        return result;
+        return dtype::array<dtype::real>::Ones(points.rows(), 1).eval();
     };
 }
