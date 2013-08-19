7cfd1060d540089df9ed0b3fda83dce3f7b908c8
schansge <grosser.knuff@googlemail.com>
2013-08-19 17:17:09 +0200

    polygon example added

---
+++ examples/polygon.cpp
@@ -0,0 +1,54 @@
+// --------------------------------------------------------------------
+// This file is part of libDistMesh.
+//
+// libDistMesh is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// libDistMesh is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libDistMesh.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2013 Patrik Gebhardt
+// Contact: patrik.gebhardt@rub.de
+// --------------------------------------------------------------------
+
+#include <distmesh/distmesh.h>
+#include <fstream>
+
+int main() {
+    distmesh::dtype::array<distmesh::dtype::real> bounding_box(2, 2);
+    bounding_box << -1.0, 2.0, -1.0, 1.0;
+
+    // corner points of polygon
+    distmesh::dtype::array<distmesh::dtype::real> polygon(10, 2);
+    polygon << -0.4, -0.5, 0.4, -0.2, 0.4, -0.7,
+        1.5, -0.4, 0.9, 0.1, 1.6, 0.8, 0.5, 0.5,
+        0.2, 1.0, 0.1, 0.4, -0.7, 0.7;
+
+    // create mesh
+    auto mesh = distmesh::distmesh(
+        distmesh::distance_function::polygon(polygon),
+        0.1, 1.0, bounding_box, polygon);
+
+    // plot mesh
+    std::ofstream points_file;
+    std::ofstream triangulation_file;
+    points_file.open("points.txt");
+    triangulation_file.open("triangulation.txt");
+
+    points_file << std::get<0>(mesh);
+    triangulation_file << std::get<1>(mesh);
+
+    points_file.close();
+    triangulation_file.close();
+
+    system("python plot_mesh.py");
+
+    return 0;
+}
